a93a49b00e922a717e9872f963c7ee3cd2970843
Sam <sammt929@gmail.com>
2025-08-13 15:07:25 -0700

    Basic boilerplate of html and css was added

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { InvestmentResultsComponent } from '../investment-results/investment-results.component';
+import { UserInputComponent } from '../user-input/user-input.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, InvestmentResultsComponent, UserInputComponent],
+  standalone: true,
+  templateUrl: './app.component.html',
+})
+export class AppComponent {}
